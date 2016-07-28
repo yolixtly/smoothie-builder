05e7152ef81dee3cea30c2df08a374c5ea68df2d
yolixtly <yolixtly_13@hotmail.com>
2016-07-28 16:42:45 -0400

    html added

---
+++ app.js
@@ -0,0 +1,21 @@
+// STEP 1 : 
+	//Create Pantry Object
+	var pantry = {
+    green: ['2 cups spinach', '1 cup kale', '3/4 cup raw broccoli'],
+    fruit: ['1 apple', '1 cup berries', '1 banana'],
+    liquid: ['1 cup greek yogurt', '1 cup almond milk', '1 cup soy milk']
+};
+	//Smoothie Object: this contains all the ingredients of 1 smoothie
+
+// $('option').text(pantry.green[0]);
+
+
+// Step 1 : get the Request / data 
+
+function getData(){
+	var url = ''
+}
+
+// Step 2 : A function that is incharge of rendering the info into html 
+
+// Step 3 : Event handler 
